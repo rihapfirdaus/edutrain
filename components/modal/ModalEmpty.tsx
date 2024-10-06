7c9ceab3cef6ae8eea4ca027b0e04cfd4f231090
rihapfirdaus <muhammadrihap448@gmail.com>
2024-10-06 20:52:32 +0700

    feat: change size

--- components/modal/ModalEmpty.tsx
+++ components/modal/ModalEmpty.tsx
@@ -15,7 +15,7 @@ export default function ModalEmpty({ message }: ModalEmptyProps) {
       className="flex flex-col gap-4 justify-center items-center text-white bg-cover bg-center rounded-3xl p-12 h-fit"
     >
       <EmptyIcon size={58} />
-      <p className="text-2xl font-bold">{message}</p>
+      <p className="text-2xl font-bold text-center">{message}</p>
       <Link
         href={"/"}
         className="px-4 py-2 bg-[#0041A1] text-white rounded-lg shadow-md hover:scale-105"
