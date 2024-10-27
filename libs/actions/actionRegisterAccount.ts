8116104ff317c0e6e66467d38cf2be2dce1d1fc7
rihapfirdaus <muhammadrihap448@gmail.com>
2024-10-27 19:31:59 +0700

    feat: implement modularization on api action

---
+++ libs/actions/actionRegisterAccount.ts
@@ -0,0 +1,40 @@
+import axiosInstance from "@/utils/axiosInstance";
+import { loadingService } from "../services/LoadingService";
+import { modalService } from "../services/ModalService";
+import { ErrorMessage } from "../entities/Error";
+
+export async function actionRegisterAccount(formData: FormData) {
+  loadingService.showLoading();
+  try {
+    const loginRequest = {
+      fullname: formData.get("fullname"),
+      username: formData.get("username"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+    };
+
+    if (loginRequest.password != formData.get("confirm")) {
+      return {
+        status: 400,
+        message: "Password yang Anda berikan tidak sama!",
+      };
+    }
+
+    const { status } = await axiosInstance.post("/auth/register", loginRequest);
+
+    if (status === 201) {
+      loadingService.hideLoading();
+      modalService.showModal({ message: "Register sukses!", type: "success" });
+    }
+  } catch (e: any) {
+    loadingService.hideLoading();
+    if (e.status === 400) {
+      modalService.showModal({
+        message: "Email telah digunakan!",
+        type: "error",
+      });
+    } else if (e.status === 500) {
+      modalService.showModal({ message: ErrorMessage.System, type: "error" });
+    }
+  }
+}
---
+++ libs/actions/actionRegisterEvent.ts
@@ -0,0 +1,40 @@
+import axiosInstance from "@/utils/axiosInstance";
+import { auth } from "./tokenHandler";
+import { modalService } from "../services/ModalService";
+import { ErrorMessage } from "../entities/Error";
+
+export async function actionRegisterEvent(formData: FormData) {
+  const isAuth = await auth();
+
+  try {
+    if (isAuth) {
+      const registerRequest = {
+        eventId: formData.get("eventId"),
+      };
+
+      const { status } = await axiosInstance.post(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/orders/user`,
+        registerRequest
+      );
+
+      if (status === 201) {
+        modalService.showModal({
+          message: "Pendaftaran berhasil!",
+          type: "success",
+        });
+      }
+    } else {
+      modalService.showModal({
+        message: "Silakan login terlebih dahulu!",
+        type: "error",
+        link: "/login",
+      });
+    }
+  } catch (e: any) {
+    modalService.showModal({
+      message: ErrorMessage.System,
+      type: "error",
+      link: "/login",
+    });
+  }
+}
