f48907f12b410271d9d3246d1332a92d02778453
rihapfirdaus <muhammadrihap448@gmail.com>
2024-11-02 11:17:05 +0700

    style: improve styling

--- components/provider/LoadingProvider.tsx
+++ components/provider/LoadingProvider.tsx
@@ -28,13 +28,15 @@ const LoadingProvider: React.FC<{ children: React.ReactNode }> = ({
         <div
           className={`fixed top-0 bottom-0 left-0 right-0 z-30 grid place-items-center transition-all duration-500`}
         >
-          <Image
-            src={"/edutrain_loading.svg"}
-            alt="loading..."
-            width={240}
-            height={100}
-            className="drop-shadow-xl select-none pointer-events-none"
-          />
+          <div className="h-fit w-fit py-4 px-8 bg-secondary flex justify-center items-center bg-opacity-65 backdrop-blur-sm rounded-xl border border-white">
+            <Image
+              src={"/edutrain_loading.svg"}
+              alt="loading..."
+              width={240}
+              height={100}
+              className="drop-shadow-xl select-none pointer-events-none"
+            />
+          </div>
         </div>
       )}
     </div>
