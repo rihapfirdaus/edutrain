fb8dd6a4dca94f2ceee20fc7950ff05cad37686b
rihapfirdaus <muhammadrihap448@gmail.com>
2024-11-02 10:33:08 +0700

    feat: add validation before register event

--- libs/actions/actionRegisterEvent.ts
+++ libs/actions/actionRegisterEvent.ts
@@ -2,38 +2,50 @@ import axiosInstance from "@/utils/axiosInstance";
 import { auth } from "./tokenHandler";
 import { modalService } from "../services/ModalService";
 import { ErrorMessage } from "../entities/Error";
+import { capitalizeEachWord } from "../helpers/formatter/stringFormatter";
+import { saveToSessionStorage } from "./storageHandler";
 
 export async function actionRegisterEvent(formData: FormData) {
   const isAuth = await auth();
 
-  try {
-    if (isAuth) {
-      const registerRequest = {
-        eventId: formData.get("eventId"),
-      };
+  const userResponse = await modalService.showModal({
+    message: capitalizeEachWord(
+      `Daftar ${formData.get("eventType")} ${formData.get("eventTitle")}?`
+    ),
+    type: "validation",
+  });
 
-      const { status } = await axiosInstance.post(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}/orders/user`,
-        registerRequest
-      );
+  if (userResponse) {
+    try {
+      if (isAuth) {
+        const registerRequest = {
+          eventId: formData.get("eventId"),
+        };
 
-      if (status === 201) {
+        const { status, data } = await axiosInstance.post(
+          `${process.env.NEXT_PUBLIC_API_BASE_URL}/orders/user`,
+          registerRequest
+        );
+
+        if (status === 201) {
+          saveToSessionStorage(data.data.eventId, data.data.id);
+          modalService.showModal({
+            message: "Pendaftaran berhasil!",
+            type: "success",
+          });
+        }
+      } else {
         modalService.showModal({
-          message: "Pendaftaran berhasil!",
-          type: "success",
+          message: "Silakan login terlebih dahulu!",
+          type: "error",
+          link: "/auth/login",
         });
       }
-    } else {
+    } catch {
       modalService.showModal({
-        message: "Silakan login terlebih dahulu!",
+        message: ErrorMessage.System,
         type: "error",
-        link: "/auth/login",
       });
     }
-  } catch {
-    modalService.showModal({
-      message: ErrorMessage.System,
-      type: "error",
-    });
   }
 }
