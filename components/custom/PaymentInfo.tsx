869081ffb6168031bd695c561e80621585a78469
rihapfirdaus <muhammadrihap448@gmail.com>
2024-11-02 16:55:58 +0700

    feat: update component

--- components/custom/PaymentInfo.tsx
+++ components/custom/PaymentInfo.tsx
@@ -11,14 +11,14 @@ export default function PaymentInfo({ price }: PaymentInfoProps) {
       <p>{currencyFormatter(price)} ke:</p>
       <p className="flex flex-col gap-1 flex-wrap text-start border border-dashed rounded-lg p-4 text-base">
         <span className="font-bold">Yogi Saputra:</span>
-        <span>0405-01-061209506 (BRI)</span>
+        <span>0405 0106 1209 506 (BRI)</span>
       </p>
     </div>
   ) : (
     <div className="flex flex-col rounded-lg border border-dashed p-4 gap-2">
       <p className="text-sm">Lakukan pembayaran ke: </p>
       <p className="flex flex-col flex-wrap gap-1 text-base">
-        <span className="font-bold">Yogi Saputra:</span>0405-01-061209506 (BRI)
+        <span className="font-bold">Yogi Saputra:</span>0405 0106 1209 506 (BRI)
       </p>
     </div>
   );
